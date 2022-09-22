5bcb6d5bdcb3e22fe8f51265eaf11e9b265eb33d
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2022-09-22 00:00:10 -0400

    Create removeStringSpace.js

---
+++ removeStringSpace.js
@@ -0,0 +1,20 @@
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+
+
+function noSpace(x){
+  
+  
+//   let result = ""
+  
+//   for (let i = 0; i < x.length; i++) {
+//     result += x[i]
+//   }
+  
+//   const noSpace = result.replace(/ /g, '')
+ 
+  
+  return x.replace(/ /g, '')
+
+}
