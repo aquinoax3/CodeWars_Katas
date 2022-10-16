1c6092504ab689b4ba3a20a6fefd0d6292a84541
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2022-10-16 00:14:27 -0400

    Create removeExclamation.js

---
+++ removeExclamation.js
@@ -0,0 +1,22 @@
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
+
+
+function remove (string) {
+  
+  const array = string.split("")
+ 
+  if(array[array.length - 1] === "!") {
+    array.pop()
+  }
+  
+    
+    return array.join("");
+}
