87d6253307b71ca622447208d3bbc85dd5ca15f4
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-01-14 23:38:41 -0500

    Create isTheDateToday.js

---
+++ isTheDateToday.js
@@ -0,0 +1,18 @@
+// Write a simple function that takes a Date as a parameter and returns a Boolean representing whether the date is today or not.
+
+// Make sure that your function does not return a false positive by only checking the day.
+
+
+
+
+
+function isToday(date) {
+  const currentDate = new Date()
+  
+  if(currentDate.toDateString() === date.toDateString()) {
+    return true
+  } else {
+    return false
+  }
+  
+}
