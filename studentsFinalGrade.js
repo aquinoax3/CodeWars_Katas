df408c0b0c16a6b6c64f4088871d4d616da970dc
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-01-16 00:44:20 -0500

    Update studentsFinalGrade.js

--- studentsFinalGrade.js
+++ studentsFinalGrade.js
@@ -36,3 +36,16 @@ function finalGrade (exam, projects) {
   }
   
 }
+
+
+// optomized soloution 
+
+function finalGrade (exam, projects) {
+
+  
+  return exam > 90 || projects > 10 ? 100
+  : exam > 75 && projects >= 5 ? 90
+  : exam > 50 && projects >= 2 ? 75
+  : 0
+  
+}
