6b71230e22b392bb71586d40bdf223cddde9d2d9
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-02-13 00:32:49 -0500

    Create doesMyListIncludeThis.js

---
+++ doesMyListIncludeThis.js
@@ -0,0 +1,17 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+
+
+function include(arr, item) {
+    for (el of arr) {
+        if (el === item) {
+            return true
+        }
+    }
+
+    return falsed
+}
+
+
+
+
