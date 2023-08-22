094002fbf84096b389e7e413d64506e30e470c2f
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-08-22 14:42:10 -0400

    Another Solution

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -3,9 +3,15 @@
 // The order of the sequence has to stay the same.
 
 
+// Sipler Solution
+function distinct(a) {
+  return [...new Set(a)];
+}
+
+
 // Cleaner Code
 function distinct(a) {
-  return a.filter((num, idx) => a.indexOf(num) === idx );
+  return a.filter((num, idx) => a.indexOf(num) === idx);
 }
 
 
@@ -13,13 +19,13 @@ function distinct(a) {
 // first attempt
 function distinct(a) {
   const result = []
-  
+
   for (let i = 0; i < a.length; i++) {
     if (result.indexOf(a[i]) === -1) {
       result.push(a[i])
     }
   }
-  
+
   return result
-  
+
 }
