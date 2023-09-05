3835d3512d50574bd78a5c6f7c540b7f42ec2df9
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-09-05 00:08:40 -0400

    Cleaner Code

--- returnTwoHighestValueInList.js
+++ returnTwoHighestValueInList.js
@@ -8,40 +8,47 @@
 // [1, 1, 1]  =>  [1]
 // []  =>  []
 
-
-
+// Cleaner Code
 function twoHighest(arr) {
-  
+  const newArray = [...new Set(arr)].sort((a, b) => a - b).reverse()
+
+  return newArray.slice(0, 2)
+}
+
+
+// Old Code
+function twoHighest(arr) {
+
   if (arr.length === 0) {
     return []
   }
-  
+
   let firstHighest = 0
   let secondHighest = 0
-  let result  = []
-  
+  let result = []
+
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > firstHighest){
+    if (arr[i] > firstHighest) {
       firstHighest = arr[i]
     }
   }
-  
+
   if (arr.length > 1) {
-    
+
     for (let j = 0; j < arr.length; j++) {
-      if(arr[j] !== firstHighest && arr[j] > secondHighest){
+      if (arr[j] !== firstHighest && arr[j] > secondHighest) {
         secondHighest = arr[j]
       }
     }
   }
-  
+
   result.push(firstHighest)
   if (arr.length > 1) {
-    
+
     result.push(secondHighest)
   }
-  
+
   console.log(result)
   return result
 }
